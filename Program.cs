852e5fef0bf930cc27d89dc49590b07118c7a2ee
BrookeAL0012 <bbcecil17@gmail.com>
2022-06-16 14:30:15 -0500

    initial message

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>() { "Fallout 4", "The Sims 4", "Stardew Vallley", "Tetris", "Monkey Island" };
+
+            var sortedGames = games.OrderBy(x => x.Length);
+
+            foreach(var game in sortedGames)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
